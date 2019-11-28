5ef97707b801df8925eb34192b016f895d525e4d
Say.li <120011676@qq.com>
2019-11-28 13:28:51 +0800

    初步创建项目结构

--- token-server/src/main/java/com/github/qq120011676/token/server/Application.java
+++ token-server/src/main/java/com/github/qq120011676/token/server/Application.java
@@ -1,13 +1,13 @@
-package com.github.qq120011676.token;
+package com.github.qq120011676.token.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TokenServerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TokenServerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
