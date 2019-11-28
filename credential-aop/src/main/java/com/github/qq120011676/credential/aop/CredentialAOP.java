7cbf40193aaeb1ea30ec2e331719194f987706f5
Say.li <120011676@qq.com>
2019-11-28 21:04:45 +0800

    增加aop

---
+++ credential-aop/src/main/java/com/github/qq120011676/credential/aop/CredentialAOP.java
@@ -0,0 +1,9 @@
+package com.github.qq120011676.credential.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class CredentialAOP {
+}
