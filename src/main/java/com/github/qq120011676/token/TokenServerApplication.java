982dc83e1fb9185ee885122b1157804f9e96308d
Say.li <120011676@qq.com>
2019-11-28 11:37:29 +0800

    init

---
+++ src/main/java/com/github/qq120011676/token/TokenServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.qq120011676.token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/qq120011676/token/TokenServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.qq120011676.token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
