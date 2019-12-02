eaef1bfdaaee1bb23598e94af06b16312b0ca6de
Say.li <120011676@qq.com>
2019-12-02 18:04:11 +0800

    修复返回数据类型不对

--- credential-server/src/main/java/com/github/qq120011676/credential/server/controller/CredentialController.java
+++ credential-server/src/main/java/com/github/qq120011676/credential/server/controller/CredentialController.java
@@ -1,12 +1,10 @@
 package com.github.qq120011676.credential.server.controller;
 
 import com.github.qq120011676.credential.common.entity.CredentialEntity;
+import com.github.qq120011676.credential.server.entity.CredentialViewEntity;
 import com.github.qq120011676.credential.server.entity.Result;
 import com.github.qq120011676.credential.server.servicce.CredentialService;
 import com.github.qq120011676.ladybird.web.exception.RestfulExceptionHelper;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import org.springframework.boot.convert.DurationStyle;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.time.Duration;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -26,33 +26,24 @@ public class CredentialController {
     private RestfulExceptionHelper restfulExceptionHelper;
 
     @RequestMapping("get")
-    public String get(String timeout) {
+    public CredentialViewEntity get(String timeout) {
         String key = UUID.randomUUID().toString();
         CredentialEntity credential = new CredentialEntity();
         credential.setToken(key);
         if (StringUtils.hasText(timeout)) {
             credential.setTimeout(DurationStyle.SIMPLE.parse(timeout));
         }
-        return this.credentialService.set(key, credential);
+        this.credentialService.set(key, credential);
+        return new CredentialViewEntity(credential.getToken());
     }
 
     @RequestMapping("gets")
-    public JsonArray get(int number, String timeout) {
-        Duration duration = null;
-        if (StringUtils.hasText(timeout)) {
-            duration = DurationStyle.SIMPLE.parse(timeout);
-        }
-        JsonArray jsonArray = new JsonArray();
-        Gson gson=new Gson();
+    public List<CredentialViewEntity> get(int number, String timeout) {
+        List<CredentialViewEntity> credentialViews = new ArrayList<>();
         for (int i = 0; i < number; i++) {
-            String key = UUID.randomUUID().toString();
-            CredentialEntity credential = new CredentialEntity();
-            credential.setToken(key);
-            credential.setTimeout(duration);
-            String jsonStr =this.credentialService.set(key, credential);
-            jsonArray.add(gson.fromJson(jsonStr, JsonObject.class));
+            credentialViews.add(this.get(timeout));
         }
-        return jsonArray;
+        return credentialViews;
     }
 
     @RequestMapping("check")
---
+++ credential-server/src/main/java/com/github/qq120011676/credential/server/entity/CredentialViewEntity.java
@@ -0,0 +1,12 @@
+package com.github.qq120011676.credential.server.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CredentialViewEntity {
+    private String token;
+}
